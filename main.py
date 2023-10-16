7a546cea5e586da766d374d4806ccc0de57e1f10
Adi Vishnu Avula <95145136+adivishnu-a@users.noreply.github.com>
2023-10-16 15:40:03 +0530

    16-10-2023

--- main.py
+++ main.py
@@ -18,21 +18,10 @@ current = volume.GetMasterVolumeLevel()
 
 while cap.isOpened():
     success, image = cap.read()
-
-    # Flip the image horizontally for a mirrored view
-    # Convert the color space from BGR to RGB
     image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
-
-    # To improve performance, set this as false
     image.flags.writeable = False
-
-    # Process the image using the face_mesh model and get the result
     results = face_mesh.process(image)
-
-    # Now, we can set the image back as writeable
     image.flags.writeable = True
-
-    # Revert the color space from RGB to BGR
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
     img_h, img_w, img_c = image.shape
@@ -47,66 +36,47 @@ while cap.isOpened():
                         nose_2d = (lm.x * img_w, lm.y * img_h)
                         nose_3d = (lm.x * img_w, lm.y * img_h, lm.z * 8000)
 
-                    x, y = int(lm.x * img_w), int(lm.y * img_h)
-
-                    # Get the 2D Coordinates
+                    x, y = int(lm.x * img_w), int(lm.y * img_h)                    
                     face_2d.append([x, y])
-
-                    # Get the 3D Coordinates
                     face_3d.append([x, y, lm.z])
-
-                    # Convert it to the NumPy array
-            face_2d = np.array(face_2d, dtype=np.float64)
-
-            # Convert it to the NumPy array
+  
+            face_2d = np.array(face_2d, dtype=np.float64)            
             face_3d = np.array(face_3d, dtype=np.float64)
-
-            # The camera matrix
             focal_length = 1 * img_w
 
             cam_matrix = np.array([[focal_length, 0, img_h / 2],
                                    [0, focal_length, img_w / 2],
                                    [0, 0, 1]])
 
-            # The Distance Matrix
-            dist_matrix = np.zeros((4, 1), dtype=np.float64)
-
-            # Solve PnP
+            dist_matrix = np.zeros((4, 1), dtype=np.float64)    
             success, rot_vec, trans_vec = cv2.solvePnP(face_3d, face_2d, cam_matrix, dist_matrix)
-
-            # Get rotational matrix
             rmat, jac = cv2.Rodrigues(rot_vec)
 
-            # Get angles
             angles, mtxR, mtxQ, Qx, Qy, Qz = cv2.RQDecomp3x3(rmat)
             x = angles[0] * 360
             y = angles[1] * 360
-
-            # Now, based on the tilt angle, set the direction and make necessary changes to the volume
+            
             text=""
-            if current<-0.15 and current>-80:
-                if y < -10:
-                    text = "Looking Left"   # user looking left, so volume focus right-side
-                    volume.SetChannelVolumeLevel(0, current - (-y / 10), None)  # Left
-                    volume.SetChannelVolumeLevel(1, current + (-y / 10), None)  # Right
-                elif y > 10:
-                    text = "Looking Right"  # user looking right, so volume focus left-side
-                    volume.SetChannelVolumeLevel(0, current + (y / 10), None)  # Left
-                    volume.SetChannelVolumeLevel(1, current - (y / 10), None)  # Right
-                elif x < -10:
-                    text = "Looking Down"   # user looking down, so volume focus decreased
-                    volume.SetMasterVolumeLevel(current - 6.0, None)
-                elif x > 10:
-                    text = "Looking Up"     # user looking up, so volume focus decreased
-                    volume.SetMasterVolumeLevel(current - 6.0, None)
-                else:
-                    text = "Forward"    # user looking forward, so volume focus increased and equalised
-                    volume.SetMasterVolumeLevel(current, None)
-                    volume.SetChannelVolumeLevel(0, current, None)  # Left
-                    volume.SetChannelVolumeLevel(1, current, None)  # Right
+            if y < -10:
+                text = "Looking Left"   
+                volume.SetChannelVolumeLevelScalar(0, (0.5 + y/100), None)  
+                volume.SetChannelVolumeLevelScalar(1, (0.5 - y/100), None)  
+            elif y > 10:
+                text = "Looking Right"  
+                volume.SetChannelVolumeLevelScalar(0, (0.5 + y/100), None)  
+                volume.SetChannelVolumeLevelScalar(1, (0.5 - y/100), None)  
+            elif x < -10:
+                text = "Looking Down"   
+                volume.SetMasterVolumeLevelScalar(0.5, None)
+            elif x > 10:
+                text = "Looking Up"     
+                volume.SetMasterVolumeLevelScalar(0.5, None)
+            else:
+                text = "Forward"    
+                volume.SetChannelVolumeLevelScalar(0, 0.5, None)  
+                volume.SetChannelVolumeLevelScalar(1, 0.5, None)  
 
-            # Display the tilt direction on the image
-            text=text+"     "+str(y)
+            text=text+"     "
             cv2.putText(image, text, (20, 20), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
 
     cv2.imshow('iCho', image)
